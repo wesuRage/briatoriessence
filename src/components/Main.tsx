a4ed389271b63fb167e1fc79f2d87cad3f6495c7
wesuRage <weslley.nmiranda@gmail.com>
2025-03-08 09:29:25 -0300

    feat(spacing): added prop for custom spacing

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,12 @@
 interface MainProps {
     children: React.ReactNode;
+    noSpace?: boolean;
 }
 
-export default function Main({children}: MainProps) {
+export default function Main({children, noSpace}: MainProps) {
     return (
         <main className="p-4">
+            <div className={`${ noSpace? "" : "mt-[134px]"}`}></div>
             {children}
         </main>
     );
