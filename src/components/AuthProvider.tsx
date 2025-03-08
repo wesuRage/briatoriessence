182e7c623646e43b159a6ba8e78262ca6852853c
wesuRage <weslley.nmiranda@gmail.com>
2025-03-08 09:31:18 -0300

    feat(auth): added auth provider

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -1,6 +1,11 @@
 "use client";
+
 import { SessionProvider } from "next-auth/react";
 
-export function AuthProvider({ children }: { children: React.ReactNode }) {
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+export default function AuthProvider({ children }: AuthProviderProps) {
   return <SessionProvider>{children}</SessionProvider>;
 }
