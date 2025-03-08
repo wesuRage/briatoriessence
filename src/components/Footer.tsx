f8f65b42b3740d5716e4fe540dd86a1d807b92b3
wesuRage <weslley.nmiranda@gmail.com>
2025-03-08 09:30:00 -0300

    feat(image): added centered logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,20 @@
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Footer() {
-    return (
-        <footer className="bg-[var(--primary)] p-4 md:p-6 flex flex-col justify-center items-center">
-            <Image width={150} height={150} src="/logo.png" alt="logo briatori essense"/>
-            <section>
-            
-            </section>
-        </footer>
-    )
+  return (
+    <footer className="bg-[var(--primary)] flex flex-col justify-center items-center border-t-2 border-[var(--primary)]">
+      <div className="p-4 md:p-6 w-full bg-black"></div>
+      <div>
+        <Image
+          width={150}
+          height={150}
+          draggable={false}
+          src="/logo.png"
+          alt="logo briatori essence"
+          className="select-none"
+        />
+        <section></section>
+      </div>
+    </footer>
+  );
 }
