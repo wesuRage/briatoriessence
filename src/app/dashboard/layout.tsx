259fe1f340142b4883b3f367e94d37b8132ba4bb
wesuRage <weslley.nmiranda@gmail.com>
2025-03-08 09:33:39 -0300

    feat(layout): added custom layout

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import "../globals.css";
+import Header from "@/components/Header/Header";
+import AuthProvider from "@/components/AuthProvider";
+
+export const metadata: Metadata = {
+  title: "Briatori Essense - Perfumaria Online",
+  description: "A sua essência à um clique de distância",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br" suppressHydrationWarning={true}>
+      <body className="antialiased">
+        <AuthProvider>
+          <Header userPage={false} />
+          {children}
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}
