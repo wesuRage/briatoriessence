8b566f150b0cc9108dc085439ce713d1d0f91e4a
wesuRage <weslley.nmiranda@gmail.com>
2025-03-15 23:42:22 -0300

    feat(GET): implemented GET method

--- src/app/api/usuario/address/route.ts
+++ src/app/api/usuario/address/route.ts
@@ -1,12 +1,35 @@
+import { getServerSession } from "next-auth";
 import prisma from "../../../../../prisma";
 import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "../../auth/[...nextauth]/route";
 
 export async function POST(req: NextRequest) {
   try {
-    const { userId, rua, numero, complemento, bairro, cidade, estado, cep } =
-      await req.json();
+    const {
+      userId,
+      rua,
+      numero,
+      complemento,
+      bairro,
+      cidade,
+      estado,
+      telefone,
+      cep,
+      cpf,
+      nome,
+    } = await req.json();
 
-    if (!userId || !rua || !numero || !bairro || !cidade || !estado || !cep) {
+    if (
+      !userId ||
+      !rua ||
+      !numero ||
+      !bairro ||
+      !cidade ||
+      !estado ||
+      !cep ||
+      !cpf ||
+      !nome
+    ) {
       return NextResponse.json(
         { error: "Todos os campos obrigatórios devem ser preenchidos." },
         { status: 400 }
@@ -33,8 +56,13 @@ export async function POST(req: NextRequest) {
         cidade,
         estado,
         cep,
-        user: { connect: { id: userId } }
-      }
+        user: { connect: { id: userId } },
+      },
+    });
+
+    await prisma.user.update({
+      where: { id: userId },
+      data: { cpf, telefone, name: nome },
     });
 
     return NextResponse.json(addressOperation, { status: 200 });
@@ -42,6 +70,37 @@ export async function POST(req: NextRequest) {
     console.error(error);
     return NextResponse.json(
       { error: "Erro ao processar endereço" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET() {
+  try {
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+      return NextResponse.json({ status: "error", data: "Not authenticated" });
+    }
+
+    const email = session.user.email;
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+      include: { address: true },
+    });
+
+    if (!user) {
+      return NextResponse.json({ status: "error", data: "User not found" });
+    }
+
+    console.log(user);
+
+    return NextResponse.json({ status: "success", data: user });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { status: "error", data: "Internal Server Error" },
       { status: 500 }
     );
   }
