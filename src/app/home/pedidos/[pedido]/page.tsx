40bb3a13cf2ca78e91098bb1c053b6e05006b289
wesuRage <weslley.nmiranda@gmail.com>
2025-03-26 00:13:56 -0300

    feat(pages): created order pages

--- src/app/home/pedidos/[pedido]/page.tsx
+++ src/app/home/pedidos/[pedido]/page.tsx
@@ -2,30 +2,109 @@
 
 import Main from "@/components/Main";
 import axios from "axios";
+import Image from "next/image";
 import { useEffect, useState } from "react";
+import { FaCreditCard } from "react-icons/fa";
+import { FaPix } from "react-icons/fa6";
 
 export default function Pedido({
   params,
 }: {
-  params: Promise<{ id: string }>;
+  params: Promise<{ pedido: string }>;
 }) {
   const [pedido, setPedido] = useState<any>();
 
   useEffect(() => {
+    if (!params) return;
     params.then(async (resolvedParams) => {
       await axios
-        .get(`/api/pedidos/${resolvedParams.id}`)
+        .get(`/api/pedidos/${resolvedParams.pedido}`)
         .then((response) => setPedido(response.data.data));
     });
   }, [params]);
 
-  if (!pedido) return <h1>Carregando...</h1>
+  if (!pedido) return <h1>Carregando...</h1>;
+
+  console.log(pedido);
 
   return (
     <Main>
-        <section className="container p-6 w-full rounded-md bg-white border border-gray-300">
-            <h1>cu</h1>
-        </section>
+      <section className="flex justify-center">
+        <div className="grid grid-cols-1">
+          <h2 className="text-center text-2xl mb-4">
+            {new Date(pedido.updatedAt).toLocaleString()}
+          </h2>
+          {pedido && (
+            <section className="min-w-[600px] container bg-white p-2 my-2 shadow-md border border-gray-400 rounded-md">
+              {pedido.produtos.map((produtoPedido: any) => (
+                <section
+                  key={produtoPedido.produto.id}
+                  className="flex py-3 border-b-2 border-gray-400 last:border-b-0"
+                >
+                  <div className="relative w-[100px] h-[100px]">
+                    <Image
+                      src={produtoPedido.produto.imagens[0]}
+                      alt={produtoPedido.produto.nome}
+                      layout="fill"
+                      quality={100}
+                      objectFit="cover"
+                      loading="lazy"
+                      className="select-none rounded-md max-h-[250px] max-w-[250px]"
+                    />
+                  </div>
+                  <div className="px-2 flex-1">
+                    <h3 className="text-2xl font-semibold">
+                      {produtoPedido.produto.nome}
+                    </h3>
+                    <p className="text-lg text-gray-700">
+                      Quantidade: {produtoPedido.quantidade}
+                    </p>
+                    <p className="text-lg text-gray-700">
+                      Preço unitário: R$
+                      {produtoPedido.produto.precoOrg.toFixed(2)}
+                    </p>
+                    <p className="text-lg text-gray-700">
+                      Subtotal: R$
+                      {(
+                        produtoPedido.quantidade *
+                        produtoPedido.produto.precoOrg
+                      ).toFixed(2)}
+                    </p>
+                  </div>
+                </section>
+              ))}
+              <div>
+                
+              </div>
+
+              <div className="mt-4">
+                <h3 className="text-xl my-1 text-gray-700">
+                  Frete: R${pedido.valorFrete.toFixed(2)} ({pedido.tipoFrete})
+                </h3>
+                <h3 className="text-xl text-gray-700">
+                  Preço total: R${pedido.valorTotal.toFixed(2)}{" "}
+                  {pedido.meioPagamento == "pix" ? "(5% OFF)" : ""}
+                </h3>
+                <h3 className="text-xl text-gray-700 flex items-center gap-1">
+                  Status do pagamento:{" "}
+                  <span className="bg-[var(--primary)] p-1 rounded-md text-black">
+                    {pedido.status}
+                  </span>{" "}
+                  <span className="flex items-center">
+                    (
+                    {pedido.meioPagamento == "pix" ? (
+                      <FaPix />
+                    ) : (
+                      <FaCreditCard />
+                    )}{" "}
+                    {String(pedido.meioPagamento).toUpperCase()})
+                  </span>
+                </h3>
+              </div>
+            </section>
+          )}
+        </div>
+      </section>
     </Main>
-  )
+  );
 }
--- src/app/home/pedidos/page.tsx
+++ src/app/home/pedidos/page.tsx
@@ -2,35 +2,74 @@
 
 import Main from "@/components/Main";
 import axios from "axios";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Pedidos() {
   const [pedidos, setPedidos] = useState<any>();
+  const { data: session } = useSession();
 
   useEffect(() => {
-    axios
-      .get(`/api/pedidos`)
-      .then((response) => setPedidos(response.data.data));
-  }, []);
+    const fetchPedidos = async () => {
+      await axios
+        .get(`/api/pedidos`)
+        .then((response) => setPedidos(response.data.data));
+    };
+
+    fetchPedidos();
+  }, [session]);
 
   if (!pedidos) return <h1>Carregando...</h1>;
 
+  console.log(pedidos);
+
   return (
     <Main>
-      <section>
-        {pedidos &&
-          pedidos.map((pedido: any, pedido_index: number) => (
-            <section key={pedido_index}>
-              <h1 className="line-clamp-3">
-                {pedido.produtos &&
-                  pedido.produtos.map((produto: any, produto_index: number) => (
-                    <>
-                      <span key={produto_index}>{produto.nome}</span>,
-                    </>
-                  ))}
-              </h1>
-            </section>
-          ))}
+      <section className="flex justify-center">
+        <div className="grid grid-cols-1">
+          <h2 className="text-center text-2xl mb-4">Meus Pedidos</h2>
+          {pedidos &&
+            pedidos.map((pedido: any, pedido_index: number) => (
+              <Link
+                href={`/home/pedidos/${pedido.pagamentoId}`}
+                key={pedido_index}
+                className="min-w-[600px] container bg-white p-2 my-2 shadow-md border border-gray-400 rounded-md"
+              >
+                <h2 className="text-2xl">
+                  {new Date(pedido.updatedAt).toLocaleString()}
+                </h2>
+                <section className="flex py-3">
+                  <div className="relative w-[100px] h-[100px]">
+                    <Image
+                      src={pedido.produtos[0].imagens[0]}
+                      alt={pedido.produtos[0].nome}
+                      layout="fill"
+                      quality={100}
+                      objectFit="cover"
+                      loading="lazy"
+                      className="select-none rounded-md  max-h-[250px] max-w-[250px]"
+                    />
+                  </div>
+                  <div className="px-2">
+                    <h3 className="flex text-2xl">
+                      Total de produtos: {pedido.totalProdutos}
+                    </h3>
+                    <h3 className="text-xl my-1 text-gray-700">
+                      Frete: R${pedido.valorFrete.toFixed(2)} ({pedido.tipoFrete})
+                    </h3>
+                    <h3 className="text-xl text-gray-700">
+                      Preço total: R${pedido.valorTotal.toFixed(2)}
+                    </h3>
+                    <h3 className="text-xl text-gray-700">
+                      Status do pagamento: <span className="bg-[var(--primary)] p-1 rounded-md text-black">{pedido.status}</span>
+                    </h3>
+                  </div>
+                </section>
+              </Link>
+            ))}
+        </div>
       </section>
     </Main>
   );
