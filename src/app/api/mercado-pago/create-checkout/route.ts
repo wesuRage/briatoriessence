3a29f2875586dde93841c964204699daff7c0698
wesuRage <weslley.nmiranda@gmail.com>
2025-03-26 21:23:10 -0300

    fix(prisma): findUnique

--- src/app/api/mercado-pago/create-checkout/route.ts
+++ src/app/api/mercado-pago/create-checkout/route.ts
@@ -149,6 +149,7 @@ export async function POST(req: Request) {
         user: {
           connect: {
             id: userid,
+            email: payer.email,
           },
         },
         produtos: {
